0927275f15dcd11cd1e85dea81afa9f12e626437
Anna <anna.ulyanova.94@list.ru>
2022-07-13 18:18:17 +0300

    feature: added integration tests for db storages

---
+++ src/test/java/ru/yandex/practicum/filmorate/dbIntegrationTest/UserDBTest.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.dbIntegrationTest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class UserDBTest {
+
+    private final UserDbStorage storage;
+}
